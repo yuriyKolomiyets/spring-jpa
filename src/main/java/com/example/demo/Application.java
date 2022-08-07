f802c5c19f5d2e1a4f130405684f805f890aa1e4
yuriykolomiyets <juriy121@gmail.com>
2022-08-07 19:03:37 +0200

    e11

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    CommandLineRunner commandLineRunner (IStudentRepository iStudentRepository){
+        return args -> {
+            iStudentRepository.save(new Student("Mariia", "Jones", "d@ae", 1));
+        };
     }
 
 }
